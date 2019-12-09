a138f09329d07c383dd820b0fd64f4cde7ecf905
Toshikuni Fukaya <toshiq2@gmail.com>
2019-12-09 13:04:19 +0900

    avoid optimize

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use std::hash::Hasher;
 use std::time::Instant;
 
 macro_rules! hash_bench {
-    ($h: ty, $m: tt) => {
+    ($h: ty, $m: tt, $t: ty) => {
         let now = Instant::now();
         let mut n = 0;
-        for _ in 0..1000 * 1000 * 1000 {
+        for i in 0..1000 * 1000 * 1000 {
             let mut hasher = <$h>::default();
-            hasher.$m(1);
+            hasher.$m(i as $t);
             n += hasher.finish();
         }
         println!(
@@ -24,21 +24,21 @@ macro_rules! hash_bench {
 }
 
 fn main() {
-    hash_bench!(FnvHasher, write_u8);
-    hash_bench!(FnvHasher, write_u16);
-    hash_bench!(FnvHasher, write_u32);
-    hash_bench!(FnvHasher, write_u64);
-    hash_bench!(FnvHasher, write_u128);
+    hash_bench!(FnvHasher, write_u8, u8);
+    hash_bench!(FnvHasher, write_u16, u16);
+    hash_bench!(FnvHasher, write_u32, u32);
+    hash_bench!(FnvHasher, write_u64, u64);
+    hash_bench!(FnvHasher, write_u128, u128);
 
-    hash_bench!(FxHasher, write_u8);
-    hash_bench!(FxHasher, write_u16);
-    hash_bench!(FxHasher, write_u32);
-    hash_bench!(FxHasher, write_u64);
-    hash_bench!(FxHasher, write_u128);
+    hash_bench!(FxHasher, write_u8, u8);
+    hash_bench!(FxHasher, write_u16, u16);
+    hash_bench!(FxHasher, write_u32, u32);
+    hash_bench!(FxHasher, write_u64, u64);
+    hash_bench!(FxHasher, write_u128, u128);
 
-    hash_bench!(SipHasher24, write_u8);
-    hash_bench!(SipHasher24, write_u16);
-    hash_bench!(SipHasher24, write_u32);
-    hash_bench!(SipHasher24, write_u64);
-    hash_bench!(SipHasher24, write_u128);
+    hash_bench!(SipHasher24, write_u8, u8);
+    hash_bench!(SipHasher24, write_u16, u16);
+    hash_bench!(SipHasher24, write_u32, u32);
+    hash_bench!(SipHasher24, write_u64, u64);
+    hash_bench!(SipHasher24, write_u128, u128);
 }
